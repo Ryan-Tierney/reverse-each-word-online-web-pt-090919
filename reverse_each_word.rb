00538a35290ad4e5e8a41efda4d0ed1665b5efd6
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-10-14 17:46:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-  word.reverse 
-  end.join(' ')
-end
 
-reverse_each_word("coding is really fucking hard!")
 
 def reverse_each_word(sentence)
   sentence.split.each do |word|
